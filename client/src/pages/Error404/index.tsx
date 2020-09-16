64e8d36c9ae161363b71abad7164565949cebcdf
FLiotta <liottafabrizio@gmail.com>
2020-09-15 23:37:15 -0300

    Moved pages to ts

--- client/src/pages/Error404/index.tsx
+++ client/src/pages/Error404/index.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 // Project
 import './styles.scss';
 
-const Error404 = () => (
+const Error404 = (): JSX.Element => (
   <div className="error-page">
     <h1>404</h1>
     <p>Page not found.</p>
