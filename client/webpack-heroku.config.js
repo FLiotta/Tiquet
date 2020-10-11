4c9eca15bbec98dc294f680af769406d1c471a19
FLiotta <liottafabrizio@gmail.com>
2020-10-11 16:02:38 -0300

    Equaled heroku webpack to main one

--- client/webpack-heroku.config.js
+++ client/webpack-heroku.config.js
@@ -36,7 +36,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         use: {
           loader: 'file-loader',
           options: {
