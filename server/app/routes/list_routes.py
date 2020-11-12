3f695386810219ede053799cd363600a9fc95669
FLiotta <liottafabrizio@gmail.com>
2020-11-12 20:47:39 -0300

    Removed default priority from models

--- server/app/routes/list_routes.py
+++ server/app/routes/list_routes.py
@@ -91,8 +91,7 @@ def new_list_task(list_id):
         "result": {
             'id': new_task.id,
             'uid': new_task.uid,
-            'title': new_task.title,
-            'priority': new_task.priority.value,
+            'title': new_task.title
         }
     }), 200
 
